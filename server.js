dd7301fc5590baeef3c5070c48b38f10f62c1acd
isao92 <01agustin92@gmail.com>
2019-01-08 20:24:51 -0800

    add

--- server.js
+++ server.js
@@ -27,3 +27,22 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+var sequelize = require('sequelize-heroku').connect(require('sequelize'));
+
+if (sequelize) {
+    sequelize.authenticate().then( function() {
+        var config = sequelize.connectionManager.config;
+        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
+        
+        sequelize.query('SELECT 1+1 as test').then( function(res) {
+            console.log('1+1='+res[0][0].test);
+        });
+        
+    }).catch( function(err) {
+        var config = sequelize.connectionManager.config;
+        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
+    });
+} else {
+    console.log('No environnement variable found.');
+}
